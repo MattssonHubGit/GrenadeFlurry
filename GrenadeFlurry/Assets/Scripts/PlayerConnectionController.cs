fd0a0212d4c68e054d3f3236fc979357de788067
MattssonHubGit <erik.mattsson@live.se>
2019-01-12 01:40:44 +0100

    started trying to understand the high level api for networking... currently the hasLocalAuthority baffels me

---
+++ GrenadeFlurry/Assets/Scripts/PlayerConnectionController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionController : NetworkBehaviour
+{
+    [SerializeField] private GameObject playerUnitPrefab;
+
+    void Start()
+    {
+
+        //Only run on own client
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        //Spawn and set a player
+        CmdSpawnPlayer(this.gameObject);
+    }
+
+    //////////////////////////////Commands
+    //Commands are run on the server only
+    [Command]
+    private void CmdSpawnPlayer(GameObject spawner)
+    {
+        GameObject _player = Instantiate(playerUnitPrefab);
+        
+
+        NetworkServer.SpawnWithClientAuthority(_player, connectionToClient);
+    }
+
+    ///////////////////////////RPCs
+    //RPCs are called on all clients induvidually
+
+    
+}
---
+++ GrenadeFlurry/Assets/Scripts/PlayerUnitController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerUnitController : NetworkBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+   
+
+    [Header("Components")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] public MeshRenderer meshRenderer;
+    [SerializeField] private Material clientOwnerMaterial;
+
+    private void Start()
+    {
+        //If this is not my player unit
+        if (hasAuthority == false)
+        {
+            Debug.Log("PlayerUnitController::Start() - hasAuthority == false");
+            meshRenderer.material = clientOwnerMaterial;
+            return;
+        }
+
+    }
+
+    private void Update()
+    {
+        //If this is not my player unit
+        if (hasAuthority == false)
+        {
+            return;
+        }
+
+        MovementController();
+
+    }
+
+    private void MovementController()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * moveSpeed, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * -moveSpeed, ForceMode.Force);
+        }
+    }
+}
